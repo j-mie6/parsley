fb38d343a4c24a989cf53d6e76cbd3e743eb351b
J_mie6 <jw14896@bristol.ac.uk>
2018-04-03 17:01:14 +0100

    Added chain tests. Since chainr requires attempt, it doesn't quite pass the tests. The good news is I had a new intrinsic lined up for both chainr1 and chainl1 that will both make them uncurried (yay), faster (yay!) and will eliminate this behaviour by removing dependence on chainPre.

--- test/ExpressionParserTests.scala
+++ test/ExpressionParserTests.scala
@@ -1,24 +1,79 @@
+import parsley.Combinator.{chainPost, chainPre, chainl1, chainr1}
+import parsley.Parsley._
+import parsley.{Failure, Success, runParser}
+import parsley.Char.{charLift, stringLift, digit}
+
 class ExpressionParserTests extends ParsleyTest
 {
-    "chainPost" must "require an initial value" in pending
-    it must "parse all operators that follow" in pending
-    it must "apply the functions" in pending
-    it must "fail if an operator fails after consuming input" in pending
+    "chainPost" must "require an initial value" in
+    {
+        runParser(chainPost('1' #> 1, '+' #> ((x: Int) => x + 1)), "1") should be (Success(1))
+    }
+    it must "parse all operators that follow" in
+    {
+        runParser(chainPost('1' #> 1, '+' #> ((x: Int) => x + 1)), "1++++++++++++++") should not be a [Failure]
+    }
+    it must "apply the functions" in
+    {
+        runParser(chainPost('1' #> 1, '+' #> ((x: Int) => x + 1)), "1++++++++++++++") should be (Success(15))
+    }
+    it must "fail if an operator fails after consuming input" in
+    {
+        runParser(chainPost('1' #> 1, "++" #> ((x: Int) => x + 1)), "1+++++++++++++") shouldBe a [Failure]
+    }
 
-    "chainPre" must "parse an operatorless value" in pending
-    it must "parse all operators that precede a value" in pending
-    it must "fail if the final value is absent" in pending
-    it must "apply the functions" in pending
+    "chainPre" must "parse an operatorless value" in
+    {
+        runParser(chainPre('1' #> 1, '+' #> ((x: Int) => x + 1)), "1") should be (Success(1))
+    }
+    it must "parse all operators that precede a value" in
+    {
+        runParser(chainPre('1' #> 1, '+' #> ((x: Int) => x + 1)), "+++++++++++1") should not be a [Failure]
+    }
+    it must "fail if the final value is absent" in
+    {
+        runParser(chainPre('1' #> 1, '+' #> ((x: Int) => x + 1)), "+++++++++++") shouldBe a [Failure]
+    }
+    it must "apply the functions" in
+    {
+        runParser(chainPre('1' #> 1, '+' #> ((x: Int) => x + 1)), "+++++++++++1") should be (Success(12))
+    }
 
-    "chainr1" must "require an initial value" in pending
-    it must "parse all operators and values that follow" in pending
-    it must "apply the functions with correct associativity" in pending
-    it must "fail if an operator or p fails after consuming input" in pending
+    "chainr1" must "require an initial value" in
+    {
+        runParser(chainr1("11" #> 1, '+' #> ((x: Int) => (y: Int) => x + y)), "11") should be (Success(1))
+    }
+    it must "parse all operators and values that follow" in
+    {
+        runParser(chainr1("11" #> 1, '+' #> ((x: Int) => (y: Int) => x + y)), "11+11+11+11+11") should be (Success(5))
+    }
+    it must "apply the functions with correct associativity" in
+    {
+        runParser(chainr1(digit.map(_.asDigit), '%' #> ((x: Int) => (y: Int) => x % y)), "6%5%2%7") should be (Success(0))
+    }
+    it must "fail if an operator or p fails after consuming input" in
+    {
+        runParser(chainr1("11" #> 1, "++" #> ((x: Int) => (y: Int) => x + y)), "11+11+11+11+11") shouldBe a [Failure]
+        runParser(chainr1("11" #> 1, "++" #> ((x: Int) => (y: Int) => x + y)), "11++11++11++1++11") shouldBe a [Failure]
+    }
 
-    "chainl1" must "require an initial value" in pending
-    it must "parse all operators and values that follow" in pending
-    it must "apply the functions with correct associativity" in pending
-    it must "fail if an operator fails after consuming input" in pending
+    "chainl1" must "require an initial value" in
+    {
+        runParser(chainl1("11" #> 1, '+' #> ((x: Int) => (y: Int) => x + y)), "11") should be (Success(1))
+    }
+    it must "parse all operators and values that follow" in
+    {
+        runParser(chainl1("11" #> 1, '+' #> ((x: Int) => (y: Int) => x + y)), "11+11+11+11+11") should be (Success(5))
+    }
+    it must "apply the functions with correct associativity" in
+    {
+        runParser(chainl1(digit.map(_.asDigit), '%' #> ((x: Int) => (y: Int) => x % y)), "6%5%2%7") should be (Success(1))
+    }
+    it must "fail if an operator fails after consuming input" in
+    {
+        runParser(chainl1("11" #> 1, "++" #> ((x: Int) => (y: Int) => x + y)), "11+11+11+11+11") shouldBe a [Failure]
+        runParser(chainl1("11" #> 1, "++" #> ((x: Int) => (y: Int) => x + y)), "11++11++11++1++11") shouldBe a [Failure]
+    }
 
     "expression parsers" should "result in correct precedence" in pending
     they should "work for multiple operators at the same level" in pending
