406b46991f68b8ecac0d7713ddec24db203dc193
Jamie Willis <j.willis19@imperial.ac.uk>
2021-01-06 11:35:28 +0000

    All tests complete

--- src/test/scala/parsley/ResultTests.scala
+++ src/test/scala/parsley/ResultTests.scala
@@ -9,12 +9,18 @@ class ResultTests extends ParsleyTest {
     it should "return false for isFailure" in {
         Success(7).isFailure shouldBe false
     }
+    it should "not throw an exception on get" in {
+        noException should be thrownBy (Success(4).get)
+    }
 
     "Failure" should "return false for isSuccess" in {
         Failure("oops").isSuccess shouldBe false
     }
     it should "return true for isFailure" in {
         Failure("oops").isFailure shouldBe true
+    }
+    it should "throw an exception on get" in {
+        a [NoSuchElementException] should be thrownBy (Failure("oops").get)
     }
 
     "Result[A]" should "behave like Either[String, A] on success" in {
@@ -57,5 +63,40 @@ class ResultTests extends ParsleyTest {
     it should "throw an exception when it fails and converted to Try" in {
         noException should be thrownBy (Success(7).toTry.get)
         an [Exception] should be thrownBy (Failure("oops").toTry.get)
+    }
+    it should "be convertible to a possibly one element sequence" in {
+        Success(4).toSeq should have size 1
+        Failure("oops").toSeq shouldBe empty
+    }
+    it should "support contains" in {
+        Success(5).contains(5) shouldBe true
+        Success(4).contains(5) shouldBe false
+        Failure("oops").contains(5) shouldBe false
+    }
+    it should "support forall" in {
+        Success(5).forall((x: Int) => x % 2 == 0) shouldBe false
+        Success(4).forall((x: Int) => x % 2 == 0) shouldBe true
+        Failure("msg").forall((x: Int) => x % 2 == 0) shouldBe true
+    }
+    it should "support foreach" in {
+        var x = 0
+        for (y <- Failure("msg")) x = y
+        x shouldBe 0
+        for (y <- Success(3)) x = y
+        x shouldBe 3
+    }
+    it should "flatten correctly" in {
+        Success(Success(4)).flatten.isSuccess shouldBe true
+        Success(Failure("ops")).flatten.isFailure shouldBe true
+        Failure("msg").flatten.isFailure shouldBe true
+    }
+    it should "be filterable" in {
+        Success(5).filterOrElse(_ == 5, "???") shouldBe Success(5)
+        Success(5).filterOrElse(_ == 4, "should be 4!") shouldBe Failure("should be 4!")
+        Failure("oops").filterOrElse(_ == 4, "should be 4!") shouldBe Failure("oops")
+    }
+    it should "be foldable" in {
+        Success(4).fold(_ => 0, _+1) shouldBe 5
+        Failure("oops").fold(_ => 0, (x: Int) => x + 1) shouldBe 0
     }
 }
