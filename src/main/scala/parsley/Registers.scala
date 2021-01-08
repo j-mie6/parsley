7f6b69bb5021f9cb93b3e3a6aeb4aeda37629eff
Jamie Willis <j.willis19@imperial.ac.uk>
2021-01-08 14:46:00 +0000

    Fixed yet more leaky abstraction

--- src/main/scala/parsley/Registers.scala
+++ src/main/scala/parsley/Registers.scala
@@ -3,21 +3,35 @@ package parsley
 /**
   * This class is used to index registers within the mutable state.
   * Currently, there are only 4 available registers, so use them wisely!
-  * @param v The index of the register to interact with
+  *
+  * If you need more than four registers but know that they will be used
+  * at different times you can rename your register, as long as they point
+  * to the same reference. You may find the
+  * [[parsley.Parsley$.LazyParsley.cast[B]* Parsley[A].cast[B: ClassTag]: Parsley[B] ]]
+  * combinator useful to change the type of a `Reg[Any]`.
+  *
+  * @note It is undefined behaviour to use a register in multiple different
+  *       independent parsers. You should be careful to parameterise the
+  *       registers in shared parsers and allocate fresh ones for each "top-level"
+  *       parser you will run.
   */
 class Reg[A] private [Reg] {
     private [parsley] var _v: Int = -1
-    def addr: Int = {
+    private [parsley] def addr: Int = {
         assert(allocated)
         _v
     }
-    def allocated: Boolean = _v != -1
-    def allocate(v: Int): Unit = {
+    private [parsley] def allocated: Boolean = _v != -1
+    private [parsley] def allocate(v: Int): Unit = {
         assert(!allocated)
         this._v = v
     }
     //override def toString: String = s"Reg(${if (allocated) addr else "unallocated"})"
 }
 object Reg {
+    /**
+      * @tparam A The type to be contained in this register during runtime
+      * @return A new register which can contain the given type
+      */
     def make[A]: Reg[A] = new Reg
 }
