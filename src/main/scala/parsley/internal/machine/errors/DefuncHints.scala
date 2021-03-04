1e1e4eda634dc6d1678027529a068793923764e1
Jamie Willis <j.willis19@imperial.ac.uk>
2021-03-04 23:26:07 +0000

    Added the incorporatedAfter check, which ensures that duplicated work is never performed

--- src/main/scala/parsley/internal/machine/errors/DefuncHints.scala
+++ src/main/scala/parsley/internal/machine/errors/DefuncHints.scala
@@ -5,12 +5,8 @@ import parsley.internal.errors.{ErrorItem, Desc}
 import scala.collection.mutable
 import scala.annotation.tailrec
 
-// TODO: After this system is in place, we need a similar one which tracks what indices of each object
-// have made it into the final value already. If an object is encountered again (i.e. during a merge)
-// we can safely discard the computation of all values which have already appeared in the output: this
-// reduces the potential complexity of the evaluator down from O(2^n) down to O(n)
-private [machine] sealed abstract class DefuncHints {
-    private [errors] val size: Int
+private [machine] sealed abstract class DefuncHints(private [errors] val size: Int) {
+    private var incorporatedAfter: Int = size
     private [errors] def nonEmpty: Boolean = size != 0
     private [errors] def isEmpty: Boolean = size == 0
     private [machine] def toSet: Set[ErrorItem] = {
@@ -18,46 +14,42 @@ private [machine] sealed abstract class DefuncHints {
         collect(set, 0)
         set.toSet
     }
-    @tailrec final private [errors] def collect(set: mutable.Set[ErrorItem], skipNext: Int): Unit = this match {
-        case EmptyHints =>
-        case self: PopHints => self.hints.collect(set, skipNext + 1)
-        case self: ReplaceHint =>
-            if (skipNext > 0) self.hints.collect(set, skipNext)
-            else self.hints.collect(set += Desc(self.label), skipNext+1)
-        case self: MergeHints =>
-            if (self.oldHints.size < skipNext) self.newHints.collect(set, skipNext - self.oldHints.size)
-            else {
-                self.oldHints.collectNonTail(set, skipNext)
-                self.newHints.collect(set, 0)
-            }
-        case self: AddError =>
-            if (skipNext - self.hints.size <= 0) self.err.collectHints(set)
-            self.hints.collect(set, skipNext)
+    @tailrec final private [errors] def collect(set: mutable.Set[ErrorItem], skipNext: Int): Unit = if (skipNext < incorporatedAfter) {
+        // This error only needs to provide the first `skipNext` elements if we encounter it again
+        incorporatedAfter = skipNext
+        this match {
+            case EmptyHints =>
+            case self: PopHints => self.hints.collect(set, skipNext + 1)
+            case self: ReplaceHint =>
+                if (skipNext > 0) self.hints.collect(set, skipNext)
+                else self.hints.collect(set += Desc(self.label), skipNext+1)
+            case self: MergeHints =>
+                if (self.oldHints.size < skipNext) self.newHints.collect(set, skipNext - self.oldHints.size)
+                else {
+                    self.oldHints.collectNonTail(set, skipNext)
+                    self.newHints.collect(set, 0)
+                }
+            case self: AddError =>
+                if (skipNext - self.hints.size <= 0) self.err.collectHints(set)
+                self.hints.collect(set, skipNext)
+        }
     }
     final private def collectNonTail(set: mutable.Set[ErrorItem], skipNext: Int): Unit = collect(set, skipNext)
 }
 
-private [machine] case object EmptyHints extends DefuncHints {
-    val size = 0
-}
+private [machine] case object EmptyHints extends DefuncHints(size = 0)
 
-private [machine] case class PopHints private (hints: DefuncHints) extends DefuncHints {
-    val size = hints.size - 1
-}
+private [machine] case class PopHints private (hints: DefuncHints) extends DefuncHints(size = hints.size - 1)
 private [machine] object PopHints {
     def apply(hints: DefuncHints): DefuncHints = if (hints.size > 1) new PopHints(hints) else EmptyHints
 }
 
-private [errors] case class ReplaceHint private (label: String, hints: DefuncHints) extends DefuncHints {
-    val size = hints.size
-}
+private [errors] case class ReplaceHint private (label: String, hints: DefuncHints) extends DefuncHints(size = hints.size)
 private [machine] object ReplaceHint {
     def apply(label: String, hints: DefuncHints): DefuncHints = if (hints.nonEmpty) new ReplaceHint(label, hints) else hints
 }
 
-private [errors] case class MergeHints private (oldHints: DefuncHints, newHints: DefuncHints) extends DefuncHints {
-    val size = oldHints.size + newHints.size
-}
+private [errors] case class MergeHints private (oldHints: DefuncHints, newHints: DefuncHints) extends DefuncHints(size = oldHints.size + newHints.size)
 private [machine] object MergeHints {
     def apply(oldHints: DefuncHints, newHints: DefuncHints): DefuncHints = {
         if (oldHints.isEmpty) newHints
@@ -66,6 +58,4 @@ private [machine] object MergeHints {
     }
 }
 
-private [machine] case class AddError(hints: DefuncHints, err: DefuncError) extends DefuncHints {
-    val size = hints.size + 1
-}
+private [machine] case class AddError(hints: DefuncHints, err: DefuncError) extends DefuncHints(size = hints.size + 1)
--- src/test/scala/parsley/internal/machine/errors/DefuncHintsTests.scala
+++ src/test/scala/parsley/internal/machine/errors/DefuncHintsTests.scala
@@ -57,8 +57,8 @@ class DefuncHintsTests extends ParsleyTest {
         MergeHints(hints1, hints2).toSet should contain only (Desc("a"), Desc("b"), Desc("d"))
     }
     it should "ensure that if the left needs complete popping that is ok" in {
-        val hints1 = AddError(AddError(EmptyHints, mkErr("a")), mkErr("b"))
-        val hints2 = AddError(AddError(EmptyHints, mkErr("c")), mkErr("d"))
+        def hints1 = AddError(AddError(EmptyHints, mkErr("a")), mkErr("b"))
+        def hints2 = AddError(AddError(EmptyHints, mkErr("c")), mkErr("d"))
         PopHints(PopHints(PopHints(MergeHints(hints1, hints2)))).toSet should contain only (Desc("d"))
         PopHints(PopHints(MergeHints(PopHints(hints1), hints2))).toSet should contain only (Desc("d"))
     }
