a64bc2f19503f558aed2fe8a2d1f8f12c4746c17
Jamie Willis <j.willis19@imperial.ac.uk>
2021-03-06 23:34:45 +0000

    Added ascii flag to combinator, just needs to be hooked in

--- src/main/scala/parsley/debug.scala
+++ src/main/scala/parsley/debug.scala
@@ -13,14 +13,17 @@ object debug {
     case object FullBreak extends Breakpoint
 
     /** This class enables the `debug` combinator on parsers */
-    implicit class DebugCombinators[P, +A](val p: P)(implicit val con: P => Parsley[A]) {
+    implicit class DebugCombinators[P, A](val p: P)(implicit val con: P => Parsley[A]) {
         /**
           * Using this method enables debugging functionality for this parser. When it is entered a snapshot is taken and
           * presented on exit. It will signify when a parser is entered and exited as well. Use the break parameter to halt
           * execution on either entry, exit, both or neither.
           * @param name The name to be assigned to this parser
+          * @param ascii Whether to render without colour (default false: render colours)
           * @param break The breakpoint properties of this parser, defaults to NoBreak
           */
-          def debug[A_ >: A](name: String, break: Breakpoint = NoBreak): Parsley[A_] = new Parsley(new deepembedding.Debug[A_](p.internal, name, break))
+        def debug(name: String, ascii: Boolean = false, break: Breakpoint = NoBreak): Parsley[A] = {
+            new Parsley(new deepembedding.Debug[A](p.internal, name, break))
+        }
     }
 }
