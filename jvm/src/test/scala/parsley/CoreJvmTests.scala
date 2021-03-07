1ae1bc7d9b8ad560bdb53ca77469e1cc6bd7d533
Jamie Willis <j.willis19@imperial.ac.uk>
2021-03-07 16:34:07 +0000

    Moved io into JVM specific code, we can't use Source in scala-js

--- jvm/src/test/scala/parsley/CoreJvmTests.scala
+++ jvm/src/test/scala/parsley/CoreJvmTests.scala
@@ -2,7 +2,8 @@ package parsley
 
 import parsley.combinator.manyUntil
 import parsley.character.{anyChar, string}
-import parsley.implicits.{charLift, stringLift}
+import parsley.implicits.character.{charLift, stringLift}
+import parsley.io._
 
 import scala.language.implicitConversions
 
@@ -10,7 +11,7 @@ import java.io.File
 
 class CoreJvmTests extends ParsleyTest {
     "parseFromFile" should "work" in {
-        (manyUntil(anyChar, "Jamie Willis") *> anyChar).parseFromFile(new File("LICENSE")) shouldBe Success('\n')
+        (manyUntil(anyChar, "Jamie Willis") *> anyChar).parseFromFile(new File("LICENSE")).get shouldBe Success('\n')
     }
 
     "stack overflows" should "not occur" in {
@@ -18,6 +19,6 @@ class CoreJvmTests extends ParsleyTest {
             if (n > 0) p *> repeat(n-1, p)
             else p
         }
-        noException should be thrownBy repeat(4000, 'a').runParser("a")
+        noException should be thrownBy repeat(4000, 'a').parse("a")
     }
 }
