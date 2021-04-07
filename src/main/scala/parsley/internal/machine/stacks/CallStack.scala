6bd8ce594077a6113dff042020dd9394179a195c
Jamie Willis <j.willis19@imperial.ac.uk>
2021-04-07 16:05:17 +0100

    Removed redundant method

--- src/main/scala/parsley/internal/machine/stacks/CallStack.scala
+++ src/main/scala/parsley/internal/machine/stacks/CallStack.scala
@@ -12,7 +12,4 @@ private [machine] object CallStack extends Stack[CallStack] {
     override protected def head(xs: CallStack): ElemTy = (xs.ret, xs.instrs)
     override protected def tail(xs: CallStack): CallStack = xs.tail
     // $COVERAGE-ON$
-    @tailrec final def drop(xs: CallStack, n: Int): CallStack = {
-        if (n > 0 && !isEmpty(xs)) drop(xs.tail, n - 1) else xs
-    }
 }
