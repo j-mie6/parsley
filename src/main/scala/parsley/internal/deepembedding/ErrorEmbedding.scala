e34108343d303fa7725c2913ef763dd48d4ac9fc
Jamie Willis <j.willis19@imperial.ac.uk>
2021-04-09 18:31:37 +0100

    factored out more code into scopedunary

--- src/main/scala/parsley/internal/deepembedding/ErrorEmbedding.scala
+++ src/main/scala/parsley/internal/deepembedding/ErrorEmbedding.scala
@@ -10,8 +10,7 @@ private [parsley] final class Unexpected(private [Unexpected] val msg: String)
     extends Singleton[Nothing](s"unexpected($msg)", new instructions.Unexpected(msg)) with MZero
 
 private [parsley] final class ErrorLabel[A](_p: =>Parsley[A], private [ErrorLabel] val label: String)
-    extends Unary[A, A](_p)(c => s"$c.label($label)", ErrorLabel.empty(label)) {
-    final override val numInstrs = 2
+    extends ScopedUnary[A, A](_p, s"label($label)", ErrorLabel.empty(label), new instructions.InputCheck(_, true), new instructions.ApplyError(label)) {
     final override def optimise: Parsley[A] = p match {
         case ct@CharTok(c) if !ct.expected.contains("") => new CharTok(c, Some(label)).asInstanceOf[Parsley[A]]
         case st@StringTok(s) if !st.expected.contains("") => new StringTok(s, Some(label)).asInstanceOf[Parsley[A]]
@@ -19,27 +18,9 @@ private [parsley] final class ErrorLabel[A](_p: =>Parsley[A], private [ErrorLabe
         case ErrorLabel(p, label2) if label2 != "" => ErrorLabel(p, label)
         case _ => this
     }
-    final override def codeGen[Cont[_, +_], R](implicit ops: ContOps[Cont, R], instrs: InstrBuffer, state: CodeGenState): Cont[R, Unit] = {
-        val handler = state.freshLabel()
-        instrs += new instructions.InputCheck(handler, true)
-        p.codeGen |> {
-            instrs += new instructions.Label(handler)
-            instrs += new instructions.ApplyError(label)
-        }
-    }
 }
 private [parsley] final class ErrorExplain[A](_p: =>Parsley[A], reason: String)
-    extends Unary[A, A](_p)(c => s"$c.explain($reason)", ErrorExplain.empty(reason)) {
-    final override val numInstrs = 2
-    final override def codeGen[Cont[_, +_], R](implicit ops: ContOps[Cont, R], instrs: InstrBuffer, state: CodeGenState): Cont[R, Unit] = {
-        val handler = state.freshLabel()
-        instrs += new instructions.InputCheck(handler)
-        p.codeGen |> {
-            instrs += new instructions.Label(handler)
-            instrs += new instructions.ApplyReason(reason)
-        }
-    }
-}
+    extends ScopedUnary[A, A](_p, s"explain($reason)", ErrorExplain.empty(reason), new instructions.InputCheck(_), new instructions.ApplyReason(reason))
 
 private [parsley] final class ErrorAmend[A](_p: =>Parsley[A]) extends ScopedUnaryWithState[A, A](_p, "amend", false, ErrorAmend.empty, instructions.Amend)
 private [parsley] final class ErrorEntrench[A](_p: =>Parsley[A])
