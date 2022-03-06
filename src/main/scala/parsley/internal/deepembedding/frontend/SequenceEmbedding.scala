df3ae49afde1d4fa774fda773c0a4def68a548e5
Jamie Willis <j.willis19@imperial.ac.uk>
2022-03-06 10:14:27 +0000

    removed frontend Seq

--- src/main/scala/parsley/internal/deepembedding/frontend/SequenceEmbedding.scala
+++ src/main/scala/parsley/internal/deepembedding/frontend/SequenceEmbedding.scala
@@ -17,15 +17,9 @@ private [parsley] final class >>=[A, B](p: LazyParsley[A], private [>>=] val f: 
     override def pretty(l: String) = s"($l >>= ?)"
 }
 
-private [deepembedding] sealed abstract class Seq[A, B, Res](left: LazyParsley[A], right: =>LazyParsley[B],
-                                                             make: (StrictParsley[A], StrictParsley[B]) => StrictParsley[Res])
-    extends Binary[A, B, Res](left, right, make) {
-    val name: String
-    def pretty(l: String, r: String) = s"($l $name $r)"
+private [parsley] final class *>[A](_p: LazyParsley[_], _q: =>LazyParsley[A]) extends Binary[Any, A, A](_p, _q, new backend.*>(_, _)) {
+    override def pretty(l: String, r: String) = s"($l *> $r)"
 }
-private [parsley] final class *>[A](_p: LazyParsley[_], _q: =>LazyParsley[A]) extends Seq[Any, A, A](_p, _q, new backend.*>(_, _)) {
-    override val name = "*>"
-}
-private [parsley] final class <*[A](_p: LazyParsley[A], _q: =>LazyParsley[_]) extends Seq[A, Any, A](_p, _q, new backend.<*(_, _)) {
-    override val name = "<*"
+private [parsley] final class <*[A](_p: LazyParsley[A], _q: =>LazyParsley[_]) extends Binary[A, Any, A](_p, _q, new backend.<*(_, _)) {
+    override def pretty(l: String, r: String) = s"($l <* $r)"
 }
