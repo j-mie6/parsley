0ef0cd7eefbe8635fa1e65344d8ca4656acb351b
Jamie Willis <j.willis19@imperial.ac.uk>
2022-04-03 01:07:42 +0100

    Added extensions module

---
+++ src/main/scala/parsley/extension.scala
@@ -0,0 +1,55 @@
+package parsley
+
+import Parsley.{ite, notFollowedBy}
+import combinator.{many, option, some}
+
+/**
+  * These implicit classes can be used to extend the core combinator set of Parsley.
+  * This may mean that importing them enables combinators that can be used on ''non-`Parsley`'' types,
+  * or might enable some syntactic sugar that is not part of the core combinator "style".
+  */
+// $COVERAGE-OFF$
+object extension {
+    /**
+     * This class exposes the `<#>` combinator on functions.
+     *
+     * @param f The function that is used for the map
+     * @version 1.0.0
+     */
+    implicit final class HaskellStyleMap[-A, +B](val f: A => B) extends AnyVal {
+        /**This combinator is an alias for `map`.*/
+        def <#>(p: Parsley[A]): Parsley[B] = p.map(f)
+    }
+    /**
+     * This class exposes a ternary operator on pairs of parsers.
+     *
+     * @param pq The parsers which serve the branches of the if
+     * @param con A conversion (if required) to turn elements of `pq` into parsers
+     * @version 1.0.0
+     */
+    implicit final class LazyChooseParsley[P, +A](pq: =>(P, P))(implicit con: P => Parsley[A]) {
+        private lazy val (p, q) = pq
+        /**This combinator is an alias for `ite`.*/
+        def ?:(b: Parsley[Boolean]): Parsley[A] = ite(b, con(p), con(q))
+    }
+
+    /** This class exposes a collection of "operator-style" combinators that are
+     * plain syntactic sugar for other functionality in the library; they are potentially
+     * less readable than the combinators they replace, so should be used sparingly.
+     *
+     * @since 4.0.0
+     */
+    implicit final class OperatorSugar[P, +A](p: =>P)(implicit con: P => Parsley[A]) {
+        // TODO: doc
+        def * : Parsley[List[A]] = many(p)
+        // TODO: doc
+        def + : Parsley[List[A]] = some(p)
+        // TODO: doc
+        def unary_! : Parsley[Unit] = notFollowedBy(p)
+        // TODO: doc
+        def -[B](q: Parsley[B]): Parsley[A] = !q *> p
+        // TODO: doc
+        def ? : Parsley[Option[A]] = option(p)
+    }
+}
+// $COVERAGE-ON$
