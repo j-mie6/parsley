7b21f1908a66c7fe894628f02034496c2828b1a3
Jamie Willis <j.willis19@imperial.ac.uk>
2022-04-11 22:29:26 +0100

    debug and extension done

--- src/main/scala/parsley/debug.scala
+++ src/main/scala/parsley/debug.scala
@@ -25,7 +25,7 @@ import scala.language.implicitConversions
   */
 // $COVERAGE-OFF$
 object debug {
-    /** Base trait for breakpoints
+    /** Base trait for breakpoints.
       *
       * @group break
       */
@@ -52,22 +52,61 @@ object debug {
     case object FullBreak extends Breakpoint
 
     private [parsley] var renderAscii = false
-    /**
-      * This method can be used to disable the coloured debug output for terminals that don't support it
+    /** This method can be used to disable the coloured debug output for terminals that don't support it.
       *
       * @group ctrl
       */
     def disableColourRendering(): Unit = renderAscii = true
 
-    /** This class enables the `debug` combinator on parsers
+    /** This class enables the `debug` combinator on parsers.
       *
+      * This extension class operates on values that are convertible to parsers. It enables the use of
+      * the `debug` combinator, which can be used to trace the execution through a parser.
+      *
+      * @constructor This constructor should not be called manually, it is designed to be used via Scala's implicit resolution.
+      * @param p the value that this class is enabling methods on.
+      * @param con a conversion that allows values convertible to parsers to be used.
+      * @tparam P the type of base value that this class is used on (the conversion to `Parsley`) is summoned automatically.
       * @group comb
       */
     implicit class DebugCombinators[P, A](p: P)(implicit con: P => Parsley[A]) {
-        /**
-          * Using this method enables debugging functionality for this parser. When it is entered a snapshot is taken and
-          * presented on exit. It will signify when a parser is entered and exited as well. Use the break parameter to halt
-          * execution on either entry, exit, both or neither.
+        /** This combinator allows this parser to be debugged by providing a trace through the execution.
+          *
+          * When this combinator is entered, it will print the name assigned to the parser,
+          * as well as the current currenting input context for a few characters on either side.
+          * This parser is then executed. If it succeeded, this combinator again reports the
+          * name along with "`Good`" and the input context. If it failed, it reports the name
+          * along with "`Bad`" and the input context.
+          *
+          * When breakpoints are used, the execution of the combinator will pause on either entry,
+          * exit, or both. The parse is resumed by entering a newline on standard input. Breakpoints
+          * will cause additional information about the internal state of the parser to be reported.
+          *
+          * @example {{{
+          * scala> import parsley.debug.DebugCombinators, parsley.character.string, parsley.Parsley.attempt
+          * scala> val abc = attempt(string("abc").debug("string abc")).debug("attempt")
+          * scala> val abd = string("abd").debug("string abd")
+          * scala> val p = (abc <|> abd).debug("or")
+          * scala> p.parse("abd")
+          * >or> (1, 1): abd•
+          *              ^
+          *   >attempt> (1, 1): abd•
+          *                     ^
+          *     >string abc> (1, 1): abd•
+          *                          ^
+          *     <string abc< (1, 3): abd• Fail
+          *                            ^
+          *   <attempt< (1, 1): abd• Fail
+          *                     ^
+          *   >string abd> (1, 1): abd•
+          *                        ^
+          *   <string abd< (1, 4): abd• Good
+          *                           ^
+          * <or< (1, 4): abd• Good
+          *                 ^
+          * val res0 = Success("abd")
+          * }}}
+          *
           * @param name The name to be assigned to this parser
           * @param break The breakpoint properties of this parser, defaults to NoBreak
           * @param coloured Whether to render with colour (default true: render colours)
