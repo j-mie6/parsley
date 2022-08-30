87f3910456c00de75ddccbe4b2a26241e0e07677
Jamie Willis <j.willis19@imperial.ac.uk>
2022-08-30 08:56:04 +0100

    Start of configuration

--- src/main/scala/parsley/token/descriptions/NumericDesc.scala
+++ src/main/scala/parsley/token/descriptions/NumericDesc.scala
@@ -5,8 +5,32 @@ package parsley.token.descriptions
 
 import parsley.token.{Impl, NotRequired}
 
+sealed abstract class Presence
+object Presence {
+    case object Required extends Presence
+    case object Optional extends Presence
+    case object Illegal extends Presence
+}
+
+sealed abstract class RationalDesc
+object RationalDesc {
+    case object Illegal extends RationalDesc
+    case class Supported()
+}
+
 private [token]
-case class NumericDesc (
+case class NumericDesc (literalBreakChar: Option[Char],
+                        leadingDotAllowed: Boolean,
+                        trailingDotAllowed: Boolean,
+                        leadingZerosAllowed: Boolean,
+                        positiveSign: Presence,
+                        // generic number
+                        integerNumbersCanBeHexadecimal: Boolean,
+                        integerNumbersCanBeOctal: Boolean,
+                        integerNumbersCanBeBinary: Boolean,
+                        rationalNumbersCanBeHexadecimal: Boolean,
+                        rationalNumbersCanBeOctal: Boolean,
+                        rationalNumbersCanBeBinary: Boolean,
                        ) {
 
 }
