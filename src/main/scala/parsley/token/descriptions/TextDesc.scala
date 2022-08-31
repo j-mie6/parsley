2185a1644e003c86212b04122c75fae1562be6cb
Jamie Willis <j.willis19@imperial.ac.uk>
2022-08-31 15:53:52 +0100

    added default escape configuration adhering to the current definition

--- src/main/scala/parsley/token/descriptions/TextDesc.scala
+++ src/main/scala/parsley/token/descriptions/TextDesc.scala
@@ -19,7 +19,8 @@ object CtrlEscape {
 private [token] // TODO: remove
 case class EscapeDesc (escBegin: Char,
                        literals: Set[Char],
-                       mapped: Map[Char, Char],
+                       singleMap: Map[Char, Char],
+                       multiMap: Map[String, Char],
                        decimalEscape: NumericEscape,
                        hexadecimalEscape: NumericEscape,
                        octalEscape: NumericEscape,
@@ -27,7 +28,12 @@ case class EscapeDesc (escBegin: Char,
                        emptyEscape: Option[Char],
                        gapsSupported: Boolean,
                        ctrlEscape: CtrlEscape,
-                      )
+                      ) {
+    // TODO: this needs to be a Radix, I think we'll need parsley.collection.immutable.Radix too
+    private [token] val escMap = multiMap ++ literals.map(c => s"c" -> c) ++ singleMap.map {
+        case (k, v) => s"k" -> v
+    }
+}
 
 private [token] // TODO: remove
 case class TextDesc (escapeChars: EscapeDesc) {
@@ -37,13 +43,55 @@ private [token]
 object TextDesc {
     val plain = TextDesc(
         escapeChars = EscapeDesc(escBegin = '\\',
-                                 literals = Set(/*TODO:*/),
-                                 mapped = Map(/*TODO:*/),
+                                 literals = Set('\'', '\"', '\\'),
+                                 singleMap = Map('0' -> '\u0000',
+                                                 'a' -> '\u0007',
+                                                 'b' -> '\u0008',
+                                                 'f' -> '\u000c',
+                                                 'n' -> '\u000a',
+                                                 'r' -> '\u000d',
+                                                 't' -> '\u0009',
+                                                 'v' -> '\u000b'),
+                                 multiMap = Map("NUL" -> '\u0000',
+                                                "SOH" -> '\u0001',
+                                                "STX" -> '\u0002',
+                                                "ETX" -> '\u0003',
+                                                "EOT" -> '\u0004',
+                                                "ENQ" -> '\u0005',
+                                                "ACK" -> '\u0006',
+                                                "BEL" -> '\u0007',
+                                                "BS"  -> '\u0008',
+                                                "HT"  -> '\u0009',
+                                                "LF"  -> '\u000a',
+                                                "VT"  -> '\u000b',
+                                                "FF"  -> '\u000c',
+                                                "CR"  -> '\u000d',
+                                                "SO"  -> '\u000e',
+                                                "SI"  -> '\u000f',
+                                                "DLE" -> '\u0010',
+                                                "DC1" -> '\u0011',
+                                                "DC2" -> '\u0012',
+                                                "DC3" -> '\u0013',
+                                                "DC4" -> '\u0014',
+                                                "NAK" -> '\u0015',
+                                                "SYN" -> '\u0016',
+                                                "ETB" -> '\u0017',
+                                                "CAN" -> '\u0018',
+                                                "EM"  -> '\u0019',
+                                                "SUB" -> '\u001a',
+                                                "ESC" -> '\u001b',
+                                                "FS"  -> '\u001c',
+                                                "GS"  -> '\u001d',
+                                                "RS"  -> '\u001e',
+                                                "US"  -> '\u001f',
+                                                "SP"  -> '\u0020',
+                                                "DEL" -> '\u007f'),
                                  decimalEscape = NumericEscape.Supported(prefix = None, maxValue = 0x10ffff),
                                  hexadecimalEscape = NumericEscape.Supported(prefix = Some('x'), maxValue = 0x10ffff),
                                  octalEscape = NumericEscape.Supported(prefix = Some('o'), maxValue = 0x10ffff),
                                  binaryEscape = NumericEscape.Illegal,
                                  emptyEscape = Some('&'),
                                  gapsSupported = true,
-                                 ctrlEscape = CtrlEscape.Supported(prefix = None, mapping = Map(/*TODO:*/))))
+                                 ctrlEscape = CtrlEscape.Supported(prefix = Some('^'),
+                                                                   mapping = ('@' to '_').map(c => c -> (c - '@').toChar).toMap)))
 }
