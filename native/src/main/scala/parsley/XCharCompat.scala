395d76c6f71a1f2e3113a628874cd1801a6fa2cf
Jamie Willis <j.willis19@imperial.ac.uk>
2022-09-27 16:15:27 +0100

    Fixed capitalisation

--- native/src/main/scala/parsley/XCharCompat.scala
+++ native/src/main/scala/parsley/XCharCompat.scala
@@ -7,6 +7,6 @@ private [parsley] object XCharCompat {
     private def highSurrogateFromNormalised(cp: Int): Char = (0xd800 | ((cp >> 10) & 0x3ff)).toChar
     private def lowSurrogateFromNormalised(cp: Int): Char = (0xdc00 | (cp & 0x3ff)).toChar
 
-    def highSurrogate(codepoint: Int): Char = highSurrogateFromNormalised(codePoint - 0x10000)
-    def lowSurrogate(codepoint: Int): Char = lowSurrogateFromNormalised(codePoint - 0x10000)
+    def highSurrogate(codepoint: Int): Char = highSurrogateFromNormalised(codepoint - 0x10000)
+    def lowSurrogate(codepoint: Int): Char = lowSurrogateFromNormalised(codepoint - 0x10000)
 }
