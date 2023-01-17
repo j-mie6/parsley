8c31c43a1568169e7f8e24249d3f2c4979c57f40
Jamie Willis <j.willis19@imperial.ac.uk>
2023-01-17 19:22:08 +0000

    Added package

---
+++ parsley/shared/src/main/scala/parsley/token/errors/package.scala
@@ -0,0 +1,21 @@
+package parsley.token
+
+/**
+  * @groupprio labels 0
+  * @groupname labels Labelling and Explain Configuration
+  * @groupdesc labels
+  *
+  * @groupprio filters 10
+  * @groupname filters Filtering Configuration
+  * @groupdesc filters
+  *
+  * @groupprio doubledot 20
+  * @groupname doubledot Preventing Double Dot
+  * @groupdesc doubledot
+  *
+  * @groupprio badchar 20
+  * @groupname badchar Verifying Bad Characters
+  * @groupdesc badchar
+  *
+  */
+package object errors
