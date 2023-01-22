be4717442f441cf3685bc4b6ddffe58eb49c9da8
Jamie Willis <j.willis19@imperial.ac.uk>
2023-01-22 21:11:26 +0000

    Added position tests

--- parsley/shared/src/main/scala/parsley/position.scala
+++ parsley/shared/src/main/scala/parsley/position.scala
@@ -20,7 +20,7 @@ object position {
       * effect on the state of the parser.
       *
       * @example {{{
-      * scala> import parsley.Parsley.line, parsley.character.char
+      * scala> import parsley.position.line, parsley.character.char
       * scala> line.parse("")
       * val res0 = Success(1)
       * scala> (char('a') *> line).parse("a")
@@ -40,7 +40,7 @@ object position {
       * effect on the state of the parser.
       *
       * @example {{{
-      * scala> import parsley.Parsley.col, parsley.character.char
+      * scala> import parsley.position.col, parsley.character.char
       * scala> col.parse("")
       * val res0 = Success(1)
       * scala> (char('a') *> col).parse("a")
@@ -61,7 +61,7 @@ object position {
       * effect on the state of the parser.
       *
       * @example {{{
-      * scala> import parsley.Parsley.pos, parsley.character.char
+      * scala> import parsley.position.pos, parsley.character.char
       * scala> pos.parse("")
       * val res0 = Success((1, 1))
       * scala> (char('a') *> pos).parse("a")
@@ -78,7 +78,26 @@ object position {
 
     // this is subject to change at the slightest notice, do NOT expose
     private [parsley] val internalOffset: Parsley[Int] = new Parsley(singletons.Offset)
-    /** TODO: Document */
+
+    /** This parser returns the current offset into the input (starting at 0) without having any other effect.
+      *
+      * When this combinator is ran, no input is required, nor consumed, and
+      * the current offset into the input will always be successfully returned. It has no other
+      * effect on the state of the parser.
+      *
+      * @example {{{
+      * scala> import parsley.position.offset, parsley.character.char
+      * scala> offset.parse("")
+      * val res0 = Success(0)
+      * scala> (char('a') *> offset).parse("a")
+      * val res0 = Success(1)
+      * scala> (char('\n') *> offset).parse("\n")
+      * val res0 = Success(1)
+      * }}}
+      *
+      * @return a parser that returns the offset the parser is currently at.
+      * @note offset does not take wide unicode codepoints into account.
+      */
     val offset: Parsley[Int] = internalOffset
 
     // These are useless at 5.0.0 I think
