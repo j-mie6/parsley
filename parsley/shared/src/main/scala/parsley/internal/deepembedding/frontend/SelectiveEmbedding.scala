181c094d0abaca22c8c698c31f4a6399b8438b02
Jamie Willis <j.willis19@imperial.ac.uk>
2023-01-22 18:02:26 +0000

    Introduce `unexpectedWhenWithReason` (#151)
    * Added combinator and generalised machinery
    
    * Removed defunct combinators from lexer
    
    * documentation
    
    * tests written

--- parsley/shared/src/main/scala/parsley/internal/deepembedding/frontend/SelectiveEmbedding.scala
+++ parsley/shared/src/main/scala/parsley/internal/deepembedding/frontend/SelectiveEmbedding.scala
@@ -26,6 +26,6 @@ private [parsley] final class FilterOut[A](p: LazyParsley[A], pred: PartialFunct
 private [parsley] final class GuardAgainst[A](p: LazyParsley[A], pred: PartialFunction[A, Seq[String]]) extends Unary[A, A](p) {
     override def make(p: StrictParsley[A]): StrictParsley[A] = new backend.GuardAgainst(p, pred)
 }
-private [parsley] final class UnexpectedWhen[A](p: LazyParsley[A], pred: PartialFunction[A, String]) extends Unary[A, A](p) {
+private [parsley] final class UnexpectedWhen[A](p: LazyParsley[A], pred: PartialFunction[A, (String, Option[String])]) extends Unary[A, A](p) {
     override def make(p: StrictParsley[A]): StrictParsley[A] = new backend.UnexpectedWhen(p, pred)
 }
