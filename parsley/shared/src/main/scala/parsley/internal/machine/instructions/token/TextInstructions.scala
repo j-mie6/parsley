07e8feafc233cb99a9c13023fb8d44966c719e15
Jamie Willis <j.willis19@imperial.ac.uk>
2023-02-02 12:50:18 +0000

    Simplified the code somewhat

--- parsley/shared/src/main/scala/parsley/internal/machine/instructions/token/TextInstructions.scala
+++ parsley/shared/src/main/scala/parsley/internal/machine/instructions/token/TextInstructions.scala
@@ -14,51 +14,37 @@ import parsley.internal.machine.errors.MultiExpectedError
 
 class EscapeMapped(escTrie: Trie[Int], caretWidth: Int, expecteds: Set[ExpectItem]) extends Instr {
     def this(escTrie: Trie[Int], escs: Set[String]) = this(escTrie, escs.view.map(_.length).max, escs.map(ExpectRaw(_)))
-    // We do need to backtrack out of this if things go wrong, it's possible another escape sequence might share a lead
+    // Do not consume input on failure, it's possible another escape sequence might share a lead
     override def apply(ctx: Context): Unit = {
         ensureRegularInstruction(ctx)
         findFirst(ctx, 0, escTrie)
     }
 
-    @tailrec private def findLongest(ctx: Context, off: Int, escs: Trie[Int], bestGuess: Int, bestGuessSz: Int): Unit = {
-        escs.get("") match {
-            case Some(x) =>
-                lazy val escsNew = escs.suffixes(ctx.peekChar(off))
-                if (ctx.moreInput(off+1) && escsNew.nonEmpty) {
-                    findLongest(ctx, off+1, escsNew, x, off)
-                }
-                else {
-                    ctx.fastUncheckedConsumeChars(off)
-                    ctx.pushAndContinue(x)
-                }
-            case None =>
-                lazy val escsNew = escs.suffixes(ctx.peekChar(off))
-                if (ctx.moreInput(off+1) && escsNew.nonEmpty) {
-                    findLongest(ctx, off+1, escsNew, bestGuess, bestGuessSz)
-                }
-                else {
-                    ctx.fastUncheckedConsumeChars(bestGuessSz)
-                    ctx.pushAndContinue(bestGuess)
-                }
+    @tailrec private def findLongest(ctx: Context, off: Int, escs: Trie[Int], longestChar: Int, longestSz: Int): Unit = {
+        val (nextLongestChar, nextLongestSz) = escs.get("") match {
+            case Some(x) => (x, off)
+            case None => (longestChar, longestSz)
+        }
+        lazy val escsNew = escs.suffixes(ctx.peekChar(off))
+        if (ctx.moreInput(off+1) && escsNew.nonEmpty) findLongest(ctx, off+1, escsNew, nextLongestChar, nextLongestSz)
+        else {
+            ctx.fastUncheckedConsumeChars(nextLongestChar)
+            ctx.pushAndContinue(nextLongestSz)
         }
     }
 
     @tailrec private def findFirst(ctx: Context, off: Int, escs: Trie[Int]): Unit = {
+        lazy val escsNew = escs.suffixes(ctx.peekChar(off))
+        val couldTryMore = ctx.moreInput(off+1) && escsNew.nonEmpty
         escs.get("") match {
             case Some(x) =>
-                lazy val escsNew = escs.suffixes(ctx.peekChar(off))
-                if (ctx.moreInput(off+1) && escsNew.nonEmpty) {
-                    findLongest(ctx, off+1, escsNew, x, off)
-                }
+                if (couldTryMore) findLongest(ctx, off+1, escsNew, x, off)
                 else {
                     ctx.fastUncheckedConsumeChars(off)
                     ctx.pushAndContinue(x)
                 }
             case None =>
-                lazy val escsNew = escs.suffixes(ctx.peekChar(off))
-                if (ctx.moreInput(off+1) && escsNew.nonEmpty) {
-                    findFirst(ctx, off+1, escsNew)
-                }
+                if (couldTryMore) findFirst(ctx, off+1, escsNew)
                 else ctx.fail(new MultiExpectedError(ctx.offset, ctx.line, ctx.col, expecteds, caretWidth))
         }
     }
