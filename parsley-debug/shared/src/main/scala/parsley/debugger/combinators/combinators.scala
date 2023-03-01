420fde1911570159fad8de1bb03ab0537e3e9bc8
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-03-01 21:23:14 +0000

    license: added copyright declaration comments

--- parsley-debug/shared/src/main/scala/parsley/debugger/combinators/combinators.scala
+++ parsley-debug/shared/src/main/scala/parsley/debugger/combinators/combinators.scala
@@ -1,3 +1,6 @@
+/* SPDX-FileCopyrightText: © 2022 Parsley Contributors <https://github.com/j-mie6/Parsley/graphs/contributors>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 package parsley.debugger
 
 import parsley.Parsley
--- parsley-debug/shared/src/main/scala/parsley/debugger/objects/DebugTree.scala
+++ parsley-debug/shared/src/main/scala/parsley/debugger/objects/DebugTree.scala
@@ -1,3 +1,6 @@
+/* SPDX-FileCopyrightText: © 2022 Parsley Contributors <https://github.com/j-mie6/Parsley/graphs/contributors>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 package parsley.debugger.objects
 
 /** The tree representing a parser's parse tree.
