acf106c804f04e5cadfecfff67816d2a88f28b9b
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-04-10 11:04:36 +0100

    fix: LeaveParser now correctly handles failure cases for parsers.
    -- TODO --
    > Fix the lack of tree objects caused by something unknown?

--- parsley-debug/shared/src/main/scala/parsley/internal/deepembedding/backend/debugger/Debugged.scala
+++ parsley-debug/shared/src/main/scala/parsley/internal/deepembedding/backend/debugger/Debugged.scala
@@ -7,14 +7,18 @@ import parsley.internal.deepembedding.ContOps.{result, suspend, ContAdapter}
 import parsley.internal.deepembedding.backend.{CodeGenState, StrictParsley, Unary}
 import parsley.internal.deepembedding.backend.StrictParsley.InstrBuffer
 import parsley.internal.deepembedding.frontend.LazyParsley
+import parsley.internal.machine.instructions.Label
 import parsley.internal.machine.instructions.debugger.{AddAttempt, EnterParser, LeaveParser}
 
 private [deepembedding] final class Debugged[A]
   (origin: LazyParsley[A], val p: StrictParsley[A])
   (implicit dbgCtx: DebugContext) extends Unary[A, A] {
   override protected[backend] def codeGen[Cont[_, +_] : ContOps, R](implicit instrs: InstrBuffer, state: CodeGenState): Cont[R, Unit] = {
-    result(instrs += new EnterParser(origin)) |>
+    val handler = state.freshLabel()
+
+    result(instrs += new EnterParser(handler, origin)) |>
     suspend(p.codeGen[Cont, R]) |>
+    (instrs += new Label(handler)) |>
     (instrs += new AddAttempt) |>
     (instrs += new LeaveParser)
   }
--- parsley-debug/shared/src/main/scala/parsley/internal/machine/instructions/debugger/DebuggerInstrs.scala
+++ parsley-debug/shared/src/main/scala/parsley/internal/machine/instructions/debugger/DebuggerInstrs.scala
@@ -1,21 +1,21 @@
 package parsley.internal.machine.instructions.debugger
 
 import parsley.debugger.objects.DebugContext
-
 import parsley.internal.deepembedding.frontend.LazyParsley
 import parsley.internal.machine.Context
-import parsley.internal.machine.instructions.Instr
+import parsley.internal.machine.instructions.{Instr, InstrWithLabel}
 
 // Instructions used by the debugger itself.
-private [internal] sealed abstract class DebuggerInstr(implicit dbgCtx: DebugContext) extends Instr
+private [internal] sealed trait DebuggerInstr extends Instr
 
 private [internal] class EnterParser
-  (origin: LazyParsley[_])
-  (implicit dbgCtx: DebugContext) extends DebuggerInstr {
+  (var label: Int, origin: LazyParsley[_])
+  (implicit dbgCtx: DebugContext) extends InstrWithLabel with DebuggerInstr {
   override def apply(ctx: Context): Unit = {
     // I think we can get away with executing this unconditionally.
     dbgCtx.push(origin)
     ctx.pushCheck() // Save our location for inputs.
+    ctx.pushHandler(label) // Mark the AddAttempt instruction as an exit handler.
     ctx.inc()
   }
 }
@@ -25,10 +25,14 @@ private [internal] class LeaveParser(implicit dbgCtx: DebugContext) extends Debu
     // See above.
     dbgCtx.pop()
     ctx.checkStack = ctx.checkStack.tail // Manually pop off our debug checkpoint.
-    ctx.inc()
+
+    if (ctx.good) ctx.inc()
+    else ctx.fail()
   }
 }
 
+// Ideally we want to make this instruction extend InstrWithLabel, but I can't do that without
+// the compiler complaining.
 private [internal] class AddAttempt(implicit dbgCtx: DebugContext) extends DebuggerInstr {
   override def apply(ctx: Context): Unit = {
     val prevCheck = ctx.checkStack.offset
