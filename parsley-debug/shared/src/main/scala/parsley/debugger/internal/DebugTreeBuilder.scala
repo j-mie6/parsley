f421f832f85cfdae3795bd3ea75c47e7da7f2631
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-04-20 11:03:40 +0100

    refactor: moved SometimesEquatable to its own file

--- parsley-debug/shared/src/main/scala/parsley/debugger/internal/DebugTreeBuilder.scala
+++ parsley-debug/shared/src/main/scala/parsley/debugger/internal/DebugTreeBuilder.scala
@@ -25,25 +25,3 @@ private [parsley] case class DebugTreeBuilder(
     node
   }
 }
-
-private [debugger] sealed abstract class SometimesEquatable[A](val item: A) {
-  override def toString: String = s"$item [?=]"
-}
-
-private [debugger] final class Equatable[A](item: A)
-  extends SometimesEquatable[A](item) {
-  override def equals(obj: Any): Boolean = obj match {
-    case eq: Equatable[A] => eq.item.equals(item)
-    case _                => false
-  }
-
-  override def hashCode(): Int = item.hashCode()
-}
-
-private [debugger] final class Referential[A](item: A)
-  extends SometimesEquatable[A](item)
-
-private [debugger] object SometimesEquatable {
-  def equatable[A](item: A): SometimesEquatable[A] = new Equatable[A](item)
-  def referential[A](item: A): SometimesEquatable[A] = new Referential[A](item)
-}
