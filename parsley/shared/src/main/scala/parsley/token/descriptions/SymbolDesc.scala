864ed38c433a3aba997d5c8480fafedc31fa8cc1
Jamie Willis <j.willis19@imperial.ac.uk>
2023-04-30 17:31:05 +0100

    Added type ascription because metals was upset :(

--- parsley/shared/src/main/scala/parsley/token/descriptions/SymbolDesc.scala
+++ parsley/shared/src/main/scala/parsley/token/descriptions/SymbolDesc.scala
@@ -17,7 +17,7 @@ final case class SymbolDesc (hardKeywords: Set[String],
                              hardOperators: Set[String],
                              caseSensitive: Boolean) {
     require((hardKeywords & hardOperators).isEmpty, "there cannot be an intersection between keywords and operators")
-    private [parsley] val hardOperatorsTrie = Trie(hardOperators)
+    private [parsley] val hardOperatorsTrie: Trie[Unit] = Trie(hardOperators)
     private [parsley] def isReservedName(name: String): Boolean =
         theReservedNames.contains(if (caseSensitive) name else name.toLowerCase)
     private lazy val theReservedNames =  if (caseSensitive) hardKeywords else hardKeywords.map(_.toLowerCase)
