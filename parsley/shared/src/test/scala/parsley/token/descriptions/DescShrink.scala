49c393993d6c8c58503904e5c689919b7323d310
Jamie Willis <j.willis19@imperial.ac.uk>
2023-06-24 00:38:03 +0100

    removed nowarn which is apparently not needed

--- parsley/shared/src/test/scala/parsley/token/descriptions/DescShrink.scala
+++ parsley/shared/src/test/scala/parsley/token/descriptions/DescShrink.scala
@@ -9,15 +9,15 @@ object DescShrink {
     // TODO: shrinking logic for new esc desc stuff
     implicit val escDescShrink: Shrink[EscapeDesc] = Shrink {
         case desc@EscapeDesc(_, literals, singles, multis, _, _, _, _, _, _) =>
-            @nowarn213("cat=deprecation")
+            //@nowarn213("cat=deprecation")
             @nowarn3("cat=deprecation")
             val shrinkLiterals = for (ls <- Shrink.shrink(literals)) yield desc.copy(literals = ls)
-            @nowarn213("cat=deprecation")
+            //@nowarn213("cat=deprecation")
             @nowarn3("cat=deprecation")
             val shrinkSingles = Shrink.shrink(singles).collect {
                 case ss if ss.forall(kv => Character.isValidCodePoint(kv._2)) => desc.copy(singleMap = ss)
             }
-            @nowarn213("cat=deprecation")
+            //@nowarn213("cat=deprecation")
             @nowarn3("cat=deprecation")
             val shrinkMultis = Shrink.shrink(multis).collect {
                 case ms if ms.forall(kv => Character.isValidCodePoint(kv._2)) &&
