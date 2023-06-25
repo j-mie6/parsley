ff29ac7885cef24d03a955c63e9b7225c37fb795
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-06-25 08:31:35 +0100

    refactor: detached pretty-printer from DebugTree

--- parsley-debug/shared/src/main/scala/parsley/debugger/DebugTree.scala
+++ parsley-debug/shared/src/main/scala/parsley/debugger/DebugTree.scala
@@ -3,9 +3,6 @@
  */
 package parsley.debugger
 
-import scala.annotation.tailrec
-import scala.collection.mutable
-
 /** The tree representing a parser's parse tree.
   *
   * Initially unpopulated, it will be populated with information regarding the parser, such as
@@ -48,55 +45,6 @@ trait DebugTree {
   def fullInput: String
 
   override def toString: String =
-    prettyPrint(PrettyPrintHelper(new StringBuilder, Vector.empty)).acc.dropRight(1).toString()
-
-  // Internal pretty-printer method.
-  private def prettyPrint(helper: PrettyPrintHelper): PrettyPrintHelper = {
-    val results = parseResults.map(printParseAttempt).mkString
-    helper.bury(s"[ $parserName ]: $results")
-    printChildren(helper, nodeChildren.toList)
-    helper
-  }
-
-  // Print a parse attempt in a human-readable way.
-  private def printParseAttempt(attempt: ParseAttempt): String =
-    s"(\"${attempt.rawInput}\" [${attempt.fromPos} -> ${attempt.toPos}], ${if (attempt.success) s"Success - [ ${attempt.result.get} ]" else "Failure"})"
-
-  // Print all the children, remembering to add a blank indent for the last child.
-  @tailrec private def printChildren
-    ( helper: PrettyPrintHelper
-    , children: List[(String, DebugTree)]
-    ): Unit =
-    children match {
-      case (_, t) :: Nil =>
-        helper.bury("|", withMark = false)
-        t.prettyPrint(helper.addBlankIndent())
-        () // XXX: Silences discarded non-unit value warning.
-      case (_, t) :: xs  =>
-        helper.bury("|", withMark = false)
-        t.prettyPrint(helper.addIndent())
-        printChildren(helper, xs)
-      case Nil           => ()
-    }
+    s"DebugTree { name: $parserName ($internalName), success: ${parseResults.exists(_.success)}, children: ${nodeChildren.keys} }"
 }
 
-// Utility class for aiding in the toString method for debug trees.
-private case class PrettyPrintHelper(acc: mutable.StringBuilder, indents: Vector[String]) {
-  // Indent a string with the given indenting delimiters.
-  def bury(str: String, withMark: Boolean = true): Unit = {
-    val pretty = if (indents.isEmpty) str
-                 else if (withMark) indents.init.mkString + "+-" + str
-                      else indents.mkString + str
-
-    acc.append(pretty + "\n")
-  }
-
-  // Add a new indent delimiter to the current helper instance.
-  // The accumulator is shared between new instances.
-  def addIndent(): PrettyPrintHelper =
-    PrettyPrintHelper(acc, indents :+ "| ")
-
-  // Adds a two-blank-space indent instead for the last child of a node.
-  def addBlankIndent(): PrettyPrintHelper =
-    PrettyPrintHelper(acc, indents :+ "  ")
-}
