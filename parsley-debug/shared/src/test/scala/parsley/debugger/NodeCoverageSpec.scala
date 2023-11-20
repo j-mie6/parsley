f29198d6622fff8bd5b64e221eddc8646c7a4d2a
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-11-20 12:32:58 +0000

    test: add result tests for debugger

--- parsley-debug/shared/src/test/scala/parsley/debugger/NodeCoverageSpec.scala
+++ parsley-debug/shared/src/test/scala/parsley/debugger/NodeCoverageSpec.scala
@@ -1,16 +1,59 @@
+/*
+ * Copyright 2020 Parsley Contributors <https://github.com/j-mie6/Parsley/graphs/contributors>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 package parsley.debugger
 
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 import org.typelevel.scalaccompat.annotation.unused
-import parsley.Parsley.fresh
-import parsley.character.string
-import parsley.combinator.many
+import parsley.Parsley
+import parsley.Parsley._ // scalastyle:ignore underscore.import
+import parsley.character._ // scalastyle:ignore underscore.import
+import parsley.combinator._ // scalastyle:ignore underscore.import
 import parsley.debugger.combinator.attachDebugger
+import parsley.expr._ // scalastyle:ignore underscore.import
 
-class NodeCoverageSpec extends AnyFlatSpec {
+class NodeCoverageSpec extends AnyFlatSpec with Matchers {
     behavior of "the debugger runtime"
 
-    it should "preserve the behaviour of parsers" in {
+    private object Arithmetic {
+        val int: Parsley[BigInt] =
+            satisfy(_.isDigit)
+                .foldLeft1(BigInt(0))((acc, c) => acc * 10 + c.asDigit)
+
+        lazy val expr: Parsley[BigInt] =
+            precedence[BigInt](
+                int,
+                char('(') ~> expr <~ char(')')
+            )(
+                Ops(InfixL)(
+                    char('*') #> (_ * _),
+                    char('/') #> (_ / _)
+                ),
+                Ops(InfixL)(
+                    char('+') #> (_ + _),
+                    char('-') #> (_ - _)
+                )
+            )
+
+        lazy val prog: Parsley[List[BigInt]] =
+            many(many(satisfy("\r\n".contains(_))) ~> expr)
+    }
+
+    it should "preserve the result of parsers" in {
+        val debugMath = attachDebugger(Arithmetic.prog)
+
+        debugMath._2.parse("1+1").get.head shouldBe 2
+        debugMath._2.parse("2*3").get.head shouldBe 6
+        debugMath._2.parse("9-4").get.head shouldBe 5
+        debugMath._2.parse("6/2").get.head shouldBe 3
+
+        debugMath._2.parse("1+2+3+4+5\n2*3*4").get shouldBe List(15, 24) // scalastyle:ignore magic.number
+    }
+
+    it should "preserve the side-effecting behaviour of parsers" in {
         var hit1 = false
         var hit2 = false
         var hit3 = false
@@ -31,7 +74,7 @@ class NodeCoverageSpec extends AnyFlatSpec {
         val tree = treeF()
 
         tree.parseResults match {
-            case Some(result) => assertResult("{1}{2}")(result.rawInput)
+            case Some(result) => result.rawInput shouldBe "{1}{2}"
             case None         => fail("No result recorded.")
         }
     }
