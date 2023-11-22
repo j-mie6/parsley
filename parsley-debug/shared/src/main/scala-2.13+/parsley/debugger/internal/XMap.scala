016f28e9952ff64c421633267b01fce7d4b1a115
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-11-22 14:28:37 +0000

    fix: remove XMap, it wasn't needed

--- parsley-debug/shared/src/main/scala-2.13+/parsley/debugger/internal/XMap.scala
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2020 Parsley Contributors <https://github.com/j-mie6/Parsley/graphs/contributors>
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-package parsley.debugger.internal
-
-// Compatibility bridge for Scala 2.12
-trait XMap[K, +V] extends Map[K, V]
