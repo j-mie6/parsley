aa328cef02866b4c201b98c5f1018507834429bd
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-11-26 23:19:08 +0000

    refactor: Rename changes
    - MapAddAll is internal only now
    - apply uses partial since code paths are the same
    - addNames now has a separate overload for IterableOnce of pairs of parsers and names

--- parsley-debug/shared/src/main/scala/parsley/debugger/internal/Rename.scala
+++ parsley-debug/shared/src/main/scala/parsley/debugger/internal/Rename.scala
@@ -13,38 +13,29 @@ import parsley.internal.deepembedding.frontend.debugger.Debugged
 
 // An object with a single public apply method that allows the renaming of a
 // debugged  parser's name in order to increase the clarity of the debugger's
-// results. By default, this does nothing other than invoke the name translation
-// utility that is used to make symbolic operators.
+// results. By default, this does nothing other than get the prettified names of
+// a parser's class defined within.
 // It also requires user interaction, as it needs to collect information from the
 // classes or objects that define their parser.
 private [parsley] object Rename {
     // This will attempt to match parsers referentially.
     // This also assumes that all parsers are present (lazy or otherwise) before debugging.
     // Populating this map is only possible if the platform contains some form of implementation for
-    // parsley.debugger.utils.collectNames, which attempts to collect the name-in-code for a given
+    // parsley.debugger.util.Collector.names, which attempts to collect the name-in-code for a given
     // reference to a parser.
+    // Alternatively, users can use the `named` combinator from the debugger to weakly assign names
+    // to their parsers, or directly invoke Collector.assignName to strongly assign names to parsers.
     lazy private val collected: mutable.Map[LazyParsley[_], String] = new mutable.HashMap()
 
-    // Compatibility for Scala 2.12.
-    implicit class MapAddAll[K, V](mutMap: mutable.Map[K, V]) {
+    // Compatibility for Scala 2.12, and for convenience within the debugger for things to do with maps.
+    private [debugger] implicit class MapAddAll[K, V](mutMap: mutable.Map[K, V]) {
         def addAllFrom(collection: Map[K, V]): Unit =
             collection.foreach { case (k, v) => mutMap(k) = v }
-
-        // This is more a user-facing utility.
-        // $COVERAGE-OFF$
-        def addAllFrom(collection: Iterable[(K, V)]): Unit =
-            collection.foreach { case (k, v) => mutMap(k) = v }
-        // $COVERAGE-ON$
     }
 
-    // This method attempts the renaming of a parser.
+    // This method attempts the renaming of a parser, even if at least partially.
     def apply(optName: Option[String], p: LazyParsley[_]): String = {
-        val defaultName = p match {
-            case dbg: Debugged[_] => dbg.origin.prettyName
-            // $COVERAGE-OFF$
-            case _ => p.prettyName // Ideally this case isn't hit at all.
-            // $COVERAGE-ON$
-        }
+        val defaultName = partial(p)
 
         val extracted = p match {
             case dbg: Debugged[_] => dbg.origin
@@ -56,16 +47,21 @@ private [parsley] object Rename {
     }
 
     // Perform the first step of renaming, a partial rename where only the type name is exposed.
-    def partial(p: LazyParsley[_]): String =
+    @inline def partial(p: LazyParsley[_]): String =
         p match {
             // $COVERAGE-OFF$
             case dbg: Debugged[_] => dbg.origin.prettyName // Ideally this case isn't hit at all.
-            // $COVERAGE-ON$
             case _ => p.prettyName
+            // $COVERAGE-ON$
         }
 
     private [parsley] def addNames(names: Map[LazyParsley[_], String]): Unit =
         collected.addAllFrom(names)
+
+    // $COVERAGE-OFF$
+    private [parsley] def addNames(names: IterableOnce[(LazyParsley[_], String)]): Unit =
+        names.iterator.foreach { case (k, v) => collected(k) = v }
+    // $COVERAGE-ON$
 
     private [parsley] def addName(par: LazyParsley[_], name: String): Unit = {
         val _ = collected.put(par, name): @unused
