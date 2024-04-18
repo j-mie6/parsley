e2d3d6020afd91d2436c1116029b217bd704558e
Jamie Willis <j.willis19@imperial.ac.uk>
2024-04-18 17:21:04 +0100

    added Scala3 parsley.debuggable annotation

---
+++ parsley-debug/shared/src/main/scala-2/parsley/debuggable.scala
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2020 Parsley Contributors <https://github.com/j-mie6/Parsley/graphs/contributors>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package parsley
+
+import scala.annotation.StaticAnnotation
+
+// TODO: implementation!
+class debuggable extends StaticAnnotation
