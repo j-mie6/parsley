2f104632df7fe484672b4e2c02c257e7b23aea8a
James Watling <james.watling@gmail.com>
2025-05-21 18:34:00 +0100

    refactor: :recycle: Use ArrayStack for shunting state and extract Goto functions

--- parsley/shared/src/main/scala/parsley/internal/machine/instructions/ShuntInstrs.scala
+++ parsley/shared/src/main/scala/parsley/internal/machine/instructions/ShuntInstrs.scala
@@ -1,11 +1,10 @@
 package parsley.internal.machine.instructions
 
-import scala.collection.mutable
-
 import parsley.XAssert._
 import parsley.internal.machine.Context
 import parsley.expr.Fixity.{PR, PO, IL, IR, IN}
 import scala.annotation.switch
+import parsley.internal.machine.stacks.ArrayStack
 
 private [internal] sealed trait ShuntInput
 
@@ -13,13 +12,13 @@ private [internal] case class Atom(v: Any, lvl: Int) extends ShuntInput
 private [internal] case class Operator(f: Any, fix: Int, prec: Int) extends ShuntInput
 
 private [internal] case class ShuntingYardState(
-  atoms: mutable.Stack[Atom],
-  operators: mutable.Stack[Operator],
+  atoms: ArrayStack[Atom],
+  operators: ArrayStack[Operator],
   var failOnNoConsumed: Boolean
 )
 
 object ShuntingYardState {
-  def empty = new ShuntingYardState(mutable.Stack.empty, mutable.Stack.empty, true)
+  def empty = new ShuntingYardState(new ArrayStack(), new ArrayStack(), true)
 }
 
 private [internal] final class Shunt(var prefixAtomLabel: Int, var postfixInfixLabel: Int, wraps: Array[Any => Any]) extends Instr {
@@ -35,11 +34,10 @@ private [internal] final class Shunt(var prefixAtomLabel: Int, var postfixInfixL
       input match {
         case a@Atom(_, _) =>
           state.atoms.push(a)
-          state.failOnNoConsumed = false
-          ctx.pc = postfixInfixLabel
+          gotoPostInfix(ctx, state)
         case o@Operator(_, fix, prec) => (fix: @switch) match {
           case PR => {
-            if (state.operators.nonEmpty && prec < state.operators.top.prec) {
+            if (!state.operators.isEmpty && prec < state.operators.peek.prec) {
               // This is a malformed expression
               val currentOffset = ctx.offset
               ctx.restoreState()
@@ -48,36 +46,33 @@ private [internal] final class Shunt(var prefixAtomLabel: Int, var postfixInfixL
               return
             }
             state.operators.push(o)
-            state.failOnNoConsumed = true
-            ctx.pc = prefixAtomLabel
+            gotoPreAtom(ctx, state)
           }
           case PO => {
-            if (state.operators.nonEmpty && state.operators.top.fix == PO && prec > state.operators.top.prec) {
+            if (!state.operators.isEmpty && state.operators.peek.fix == PO && prec > state.operators.peek.prec) {
               // This was an unexpected postfix operator
               ctx.restoreState()
               produceResult(ctx)
               return
             }
-            while (state.operators.nonEmpty && state.operators.top.prec >= prec) {
+            while (!state.operators.isEmpty && state.operators.peek.prec >= prec) {
               reduce(state)
             }
             state.operators.push(o)
-            state.failOnNoConsumed = false
-            ctx.pc = postfixInfixLabel
+            gotoPostInfix(ctx, state)
           }
           case IL => {
-            while (state.operators.nonEmpty && state.operators.top.prec >= prec) {
+            while (!state.operators.isEmpty && state.operators.peek.prec >= prec) {
               reduce(state)
             }
             state.operators.push(o)
-            state.failOnNoConsumed = true
-            ctx.pc = prefixAtomLabel
+            gotoPreAtom(ctx, state)
           }
           case IR | IN => {
-            while (state.operators.nonEmpty && state.operators.top.prec > prec) {
+            while (!state.operators.isEmpty && state.operators.peek.prec > prec) {
               reduce(state)
             }
-            if (fix == IN && state.operators.nonEmpty && state.operators.top.fix == IN && state.operators.top.prec == prec) {
+            if (fix == IN && !state.operators.isEmpty && state.operators.peek.fix == IN && state.operators.peek.prec == prec) {
               // This is a special case in which non-associative operators are chained
               val currentOffset = ctx.offset
               ctx.restoreState()
@@ -86,8 +81,7 @@ private [internal] final class Shunt(var prefixAtomLabel: Int, var postfixInfixL
               return
             }
             state.operators.push(o)
-            state.failOnNoConsumed = true
-            ctx.pc = prefixAtomLabel
+            gotoPreAtom(ctx, state)
           }
         }
       }
@@ -111,43 +105,53 @@ private [internal] final class Shunt(var prefixAtomLabel: Int, var postfixInfixL
     }
   }
 
+  private def gotoPreAtom(ctx: Context, state: ShuntingYardState): Unit = {
+    state.failOnNoConsumed = true
+    ctx.pc = prefixAtomLabel
+  }
+
+  private def gotoPostInfix(ctx: Context, state: ShuntingYardState): Unit = {
+    state.failOnNoConsumed = false
+    ctx.pc = postfixInfixLabel
+  }
+
   private def produceResult(ctx: Context): Unit = {
     val state = ctx.stack.pop[ShuntingYardState]()
     // println("State: " + state)
 
-    while (state.operators.nonEmpty) reduce(state)
+    while (!state.operators.isEmpty) reduce(state)
 
     assume(state.atoms.size == 1, "Expected exactly one atom at the end of reduction")
     
-    val Atom(v, lvl) = state.atoms.pop()
+    val Atom(v, lvl) = state.atoms.pop[Atom]()
     ctx.stack.push(wrap(lvl, 0)(v))
     ctx.handlers = ctx.handlers.tail
     ctx.inc()
   }
 
   private def reduce(state: ShuntingYardState): Unit = {
-    val op = state.operators.pop()
+    val op = state.operators.pop[Operator]()
     (op.fix: @switch) match {
       case IL => {
-        val right = state.atoms.pop()
-        val left = state.atoms.pop()
+        val right = state.atoms.pop[Atom]()
+        val left = state.atoms.pop[Atom]()
         val result = op.f.asInstanceOf[(Any, Any) => Any](wrap(left.lvl, op.prec)(left.v), wrap(right.lvl, op.prec + 1)(right.v))
         state.atoms.push(Atom(result, op.prec))
       }
       case IR => {
-        val right = state.atoms.pop()
-        val left = state.atoms.pop()
+        val right = state.atoms.pop[Atom]()
+        val left = state.atoms.pop[Atom]()
         val result = op.f.asInstanceOf[(Any, Any) => Any](wrap(left.lvl, op.prec + 1)(left.v), wrap(right.lvl, op.prec)(right.v))
         state.atoms.push(Atom(result, op.prec))
       }
       case IN => {
-        val right = state.atoms.pop()
-        val left = state.atoms.pop()
+        val right = state.atoms.pop[Atom]()
+        val left = state.atoms.pop[Atom]()
         val result = op.f.asInstanceOf[(Any, Any) => Any](wrap(left.lvl, op.prec + 1)(left.v), wrap(right.lvl, op.prec + 1)(right.v))
         state.atoms.push(Atom(result, op.prec))
       }
       case PO | PR => {
-        val right = state.atoms.pop()
+        val right = state.atoms.pop[Atom]()
         val result = op.f.asInstanceOf[Any => Any](wrap(right.lvl, op.prec)(right.v))
         state.atoms.push(Atom(result, op.prec))
       }
