1ff90deb878195743b650b7fe4e79e828f30517b
Jamie Willis <j.willis19@imperial.ac.uk>
2025-12-24 17:43:08 +0000

    refactor: removed spurious line in header

--- parsley/shared/src/main/scala-3/parsley/experimental/generic/bridge.scala
+++ parsley/shared/src/main/scala-3/parsley/experimental/generic/bridge.scala
@@ -3,7 +3,6 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-
 package parsley
 package experimental.generic
 import scala.quoted.*
@@ -11,12 +10,14 @@ import scala.quoted.*
 import bridges.ErrorBridge
 
 // TODO: consider how to incorporate the errors in cleanly, this sucks
-inline transparent def bridge[T]: ErrorBridge = bridge[T, T]
-inline transparent def bridge[T, S >: T]: ErrorBridge = ${bridgeImpl[T, S]('Nil, 'None)}
+object bridge {
+    inline transparent def apply[T]: ErrorBridge = apply[T, T]
+    inline transparent def apply[T, S >: T]: ErrorBridge = ${bridgeImpl[T, S]('Nil, 'None)}
 
-inline transparent def bridgeErr[T](labels: List[String], reason: Option[String]): ErrorBridge = bridgeErr[T, T](labels, reason)
-inline transparent def bridgeErr[T, S >: T](labels: List[String], reason: Option[String]): ErrorBridge = ${bridgeImpl[T, S]('labels, 'reason)}
+    inline transparent def err[T](labels: List[String], reason: Option[String]): ErrorBridge = err[T, T](labels, reason)
+    inline transparent def err[T, S >: T](labels: List[String], reason: Option[String]): ErrorBridge = ${bridgeImpl[T, S]('labels, 'reason)}
 
-private def bridgeImpl[T: Type, S >: T: Type](labels: Expr[List[String]], reason: Expr[Option[String]])(using Quotes): Expr[ErrorBridge] = {
-    BridgeImpl().synthesise[T, S](labels, reason)
+    private [generic] def bridgeImpl[T: Type, S >: T: Type](labels: Expr[List[String]], reason: Expr[Option[String]])(using Quotes): Expr[ErrorBridge] = {
+        BridgeImpl().synthesise[T, S](labels, reason)
+    }
 }
