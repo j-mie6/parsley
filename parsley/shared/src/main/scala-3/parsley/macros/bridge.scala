3de86b02320ab27d8ea00cbd5c6f5c14a1a5b018
Jamie Willis <j.willis19@imperial.ac.uk>
2026-01-05 23:53:11 +0000

    doc: added documentation for macro bridge

--- parsley/shared/src/main/scala-3/parsley/macros/bridge.scala
+++ parsley/shared/src/main/scala-3/parsley/macros/bridge.scala
@@ -13,39 +13,129 @@ import bridges.ErrorBridge
 // TODO: we want to have .guardAgainst/.filterOut terminal methods too for each
 // TODO: we want some kind of way to denote where exactly the metadata should be parsed after
 
-/**
+/** This object implements a macro that can synthesise parser bridges.
   *
+  * @since 5.0.0
   */
 object bridge {
+    /** Synthesises a parser bridge that allows for the combining of the
+      * correct number of sub-parsers to produce a value of type `Parsley[T]`.
+      *
+      * @tparam T the product type to bridge the arguments of
+      * @since 5.0.0
+      */
     inline transparent def apply[T]: ErrorBridge = apply[T, T]
+    /** Synthesises a parser bridge that allows for the combining of the
+      * correct number of sub-parsers to produce a value of type `Parsley[T]`
+      * and upcasts to `Parsley[S]`.
+      *
+      * @tparam T the product type to bridge the arguments of
+      * @tparam S the parent type of `T` to upcast to
+      *
+      * @since 5.0.0
+      */
     inline transparent def apply[T, S >: T]: ErrorBridge = MacroImpl.impl[T, S](Nil, None)
 
-    inline def label(label: String, labels: String*) = Labelled(label +: labels)
+    /** Returns a new bridge synthesiser, which builds bridges labelled by the
+      * given errors.
+      *
+      * @param label0 the first label
+      * @param labels any other labels
+      * @since 5.0.0
+      */
+    inline def label(label0: String, labels: String*) = Labelled(label0 +: labels)
+
+    /** Returns a new bridge synthesiser, which builds bridges explained by the given reason.
+      *
+      * @param reason explanation behind what is expected
+      * @since 5.0.0
+      */
     inline def explain(reason: String) = Explained(reason)
 }
 
 class Labelled(labels: Seq[String]) {
+    /** Synthesises a parser bridge that allows for the combining of the
+      * correct number of sub-parsers to produce a value of type `Parsley[T]`.
+      *
+      * @tparam T the product type to bridge the arguments of
+      * @note has labels applied on error
+      * @since 5.0.0
+      */
     inline transparent def apply[T]: ErrorBridge = apply[T, T]
+    /** Synthesises a parser bridge that allows for the combining of the
+      * correct number of sub-parsers to produce a value of type `Parsley[T]`
+      * and upcasts to `Parsley[S]`.
+      *
+      * @tparam T the product type to bridge the arguments of
+      * @tparam S the parent type of `T` to upcast to
+      * @note has labels applied on error
+      * @since 5.0.0
+      */
     inline transparent def apply[T, S >: T]: ErrorBridge = MacroImpl.impl[T, S](labels.toList, None)
 
+    /** Returns a new bridge synthesiser, which builds bridges explained by the given reason
+      * in addition to the existing labels.
+      *
+      * @param reason explanation behind what is expected
+      * @since 5.0.0
+      */
     inline def explain(reason: String) = LabelledAndExplained(labels, reason)
 }
 
 class Explained(reason: String) {
+    /** Synthesises a parser bridge that allows for the combining of the
+      * correct number of sub-parsers to produce a value of type `Parsley[T]`.
+      *
+      * @tparam T the product type to bridge the arguments of
+      * @note has reason applied on error
+      * @since 5.0.0
+      */
     inline transparent def apply[T]: ErrorBridge = apply[T, T]
+    /** Synthesises a parser bridge that allows for the combining of the
+      * correct number of sub-parsers to produce a value of type `Parsley[T]`
+      * and upcasts to `Parsley[S]`.
+      *
+      * @tparam T the product type to bridge the arguments of
+      * @tparam S the parent type of `T` to upcast to
+      * @note has reason applied on error
+      * @since 5.0.0
+      */
     inline transparent def apply[T, S >: T]: ErrorBridge = MacroImpl.impl[T, S](Nil, Some(reason))
 
-    inline def label(label: String, labels: String*) = LabelledAndExplained(label +: labels, reason)
+    /** Returns a new bridge synthesiser, which builds bridges labelled by the
+      * given errors as well as the already provided reason.
+      *
+      * @param label0 the first label
+      * @param labels any other labels
+      * @since 5.0.0
+      */
+    inline def label(label0: String, labels: String*) = LabelledAndExplained(label0 +: labels, reason)
 }
 
 class LabelledAndExplained(labels: Seq[String], reason: String) {
+    /** Synthesises a parser bridge that allows for the combining of the
+      * correct number of sub-parsers to produce a value of type `Parsley[T]`.
+      *
+      * @tparam T the product type to bridge the arguments of
+      * @note has labels and reason applied on error
+      * @since 5.0.0
+      */
     inline transparent def apply[T]: ErrorBridge = apply[T, T]
+    /** Synthesises a parser bridge that allows for the combining of the
+      * correct number of sub-parsers to produce a value of type `Parsley[T]`
+      * and upcasts to `Parsley[S]`.
+      *
+      * @tparam T the product type to bridge the arguments of
+      * @tparam S the parent type of `T` to upcast to
+      * @note has labels and reason applied on error
+      * @since 5.0.0
+      */
     inline transparent def apply[T, S >: T]: ErrorBridge = MacroImpl.impl[T, S](labels.toList, Some(reason))
 }
 
 private object MacroImpl {
     inline transparent def impl[T, S >: T](labels: List[String], reason: Option[String]) = ${bridgeImpl[T, S]('labels, 'reason)}
-    def bridgeImpl[T: Type, S >: T: Type](labels: Expr[List[String]], reason: Expr[Option[String]])(using Quotes): Expr[ErrorBridge] = {
+    def bridgeImpl[T: Type, S >: T: Type](labels: Expr[List[String]], reason: Expr[Option[String]])(using Quotes) = {
         BridgeImpl().synthesise[T, S](labels, reason)
     }
 }
